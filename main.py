61000380ac80a2881934aa876d447e791aa25969
Thodoris Goulas <t.goulas@paymentcomponents.com>
2021-02-03 13:52:35 +0200

    feat: Added knn classifier.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from predictors.predictors import logistic_regression
+from predictors.predictors import logistic_regression, knn_classifier
 from utils import data_handler as json
 from utils import utilities as utils
 import plotly.express as px
@@ -10,3 +10,8 @@ if __name__ == '__main__':
     print(f"Optimal logistic regression classifier solver: {lr['optimal_clf'].best_params_['solver']}")
     print(f"with optimal data preprocessing policy: {lr['optimal_preprocess_policy']}")
     print(f"Accuracy score: {lr['optimal_clf'].best_score_}")
+
+    knn = knn_classifier(data_set)
+    print(f"Optimal KNN classifier params: {knn['optimal_params']}")
+    print(f"with optimal data preprocessing policy: {knn['optimal_preprocess_policy']}")
+    print(f"Accuracy score: {knn['optimal_clf'].best_score_}")
--- predictors/predictors.py
+++ predictors/predictors.py
@@ -5,6 +5,8 @@ from sklearn.model_selection import GridSearchCV
 import plotly.graph_objects as go
 import warnings
 
+from sklearn.neighbors import KNeighborsClassifier
+
 warnings.filterwarnings('ignore')
 warnings.simplefilter('ignore')
 
@@ -75,3 +77,79 @@ def logistic_regression(data_set):
         figures[key].show()
 
     return {'optimal_clf': optimal_clf, 'optimal_preprocess_policy': optimal_preprocess_policy}
+
+
+def knn_classifier(data_set):
+    colors = ['indianred', 'lightsalmon', 'crimson', 'blue', 'green', 'purple']
+    grid_params = {
+        'n_neighbors': [3, 5, 11, 19],
+        'weights': ['uniform', 'distance'],
+        'metric': ['euclidean', 'manhattan']
+    }
+    gs = GridSearchCV(
+        KNeighborsClassifier(),
+        grid_params,
+        verbose=0,
+        cv=10,
+        n_jobs=-1
+    )
+
+    # initialize one figure per parameter
+    figures = {}
+    for key in grid_params.keys():
+        fig = go.Figure(layout={
+            'barmode': 'group',
+            'xaxis_tickangle': -45,
+            'title': {
+                'text': f"Accuracy per {key}",
+                'y': 0.9,
+                'x': 0.5,
+                'xanchor': 'center',
+                'yanchor': 'top'
+            },
+            'xaxis_title': key,
+            'yaxis_title': "Accuracy Score",
+            'legend_title': "Data preprocess algorithm"
+        })
+        figures[key] = fig
+
+    optimal_clf = None
+    optimal_preprocess_policy = 'original'
+
+    color_index = 0
+    for key in data_set.keys():
+        if key == "y":
+            continue
+        else:
+            gs.fit(data_set[key], data_set["y"])
+
+            # find optimal classifier and data scaling algorithm
+            if optimal_clf is None or optimal_clf.best_score_ < gs.best_score_:
+                optimal_clf = deepcopy(gs)
+                optimal_preprocess_policy = key
+
+            for param in grid_params.keys():
+                param_score = {}
+                for value in grid_params[param]:
+                    indices = [i for i, x in enumerate(gs.cv_results_[f"param_{param}"]) if x == value]
+                    param_score[value] = max([gs.cv_results_['mean_test_score'][i] for i in indices])
+
+                fig = figures[param]
+                fig.add_trace(go.Bar(
+                    x=list(map(str,grid_params['n_neighbors'])),
+                    y=list(param_score.values()),
+                    name=key,
+                    marker_color=colors[color_index]
+                ))
+
+        color_index += 1
+
+    for key in figures.keys():
+        figures[key].show()
+
+    optimal_params = ''
+    for key, value in optimal_clf.best_params_.items():
+        optimal_params += f"{key}: {value}, "
+    optimal_params = optimal_params[:-2]
+
+    return {'optimal_clf': optimal_clf, 'optimal_preprocess_policy': optimal_preprocess_policy, 'optimal_params': optimal_params}
